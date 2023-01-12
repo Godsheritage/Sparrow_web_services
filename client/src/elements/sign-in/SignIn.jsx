1ee32f4c677d7cdbee9d67e27a323b0d147e0a43
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-01-12 07:38:29 -0500

    update to the application login

--- client/src/elements/sign-in/SignIn.jsx
+++ client/src/elements/sign-in/SignIn.jsx
@@ -6,10 +6,11 @@ const SignIn = () => {
     <div className="sign-in-box">
       <h1>Sign In</h1>
       <div className="input-fields ">
-        <input type="text" value="" placeholder="username" className="form-control" />
+        <input type="text" value="" placeholder="username" className="form-control mb-3" />
         <input type="password" value="" placeholder="password" className="form-control"/>
       </div>
       <button className="btn btn-primary">Sign In</button>
+      <p>Don't have an account? Sign Up</p>
     </div>
   );
 };
