03978f7c3af0a80f370ecd1c09fe6da99d0d7153
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-01-11 21:26:36 -0500

    updates

--- client/src/elements/sign-in/SignIn.jsx
+++ client/src/elements/sign-in/SignIn.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import "./signin.scss"
+import React from "react";
+import "./signin.scss";
 
 const SignIn = () => {
   return (
-    <div className='sign-in-box'>
-        <h1>Sign In</h1>
-        <input type="text" value="" placeholder='username' />
-        <input type="password" value="" placeholder='password' />
-        <button>Sign In</button>
+    <div className="sign-in-box">
+      <h1>Sign In</h1>
+      <div className="input-fields ">
+        <input type="text" value="" placeholder="username" className="form-control" />
+        <input type="password" value="" placeholder="password" className="form-control"/>
+      </div>
+      <button className="btn btn-primary">Sign In</button>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
