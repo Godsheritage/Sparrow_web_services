22f7af5815a6051ae69601e7935417b0d2dfcbd7
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-01-13 08:47:37 -0500

    form updates

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,15 +1,73 @@
-import React from 'react'
-import Input from '../../components/form elements/Input'
+import React from "react";
+import Input from "../../components/form elements/Input";
 
 const Profile = () => {
   return (
-    <div className='profile'>
-        <h1>profile</h1>
-        <div className="form">
-            
-        </div>
+    <div className="profile">
+      <h1>profile</h1>
+      <div className="form">
+        <Input
+          element="input"
+          type="text"
+          placeholder="Upload Image"
+          id="username"
+          className="form-control"
+        />
+        <Input
+          element="input"
+          type="text"
+          placeholder="Business Name"
+          id="businessname"
+          className="form-control"
+        />
+        <Input
+          element="input"
+          type="text"
+          placeholder="Opening Hours"
+          id="openinghours"
+          className="form-control"
+        />
+        <Input
+          element="input"
+          type="text"
+          placeholder="Locations"
+          id="locations"
+          className="form-control"
+        />
+        <Input
+          element="input"
+          type="text"
+          placeholder="Phone Number"
+          id="phonenumber"
+          className="form-control"
+        />
+        <Input
+          element="input"
+          type="text"
+          placeholder="Business Type"
+          id="businesstype"
+          className="form-control"
+        />
+        <Input
+          element="input"
+          type="text"
+          placeholder="Business Description"
+          id="description"
+          className="form-control"
+        />
+        <Input
+          element="input"
+          type="text"
+          placeholder="Add Files / Images"
+          id="username"
+          className="form-control"
+        />
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+//Todo Create button components
+
+export default Profile;
