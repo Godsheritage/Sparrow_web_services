87bb7d92c14cb172372d60311a83c07c93e2117a
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-01-14 23:30:48 -0500

    Worked on map integration

---
+++ client/src/components/ui/map/Map.jsx
@@ -0,0 +1,27 @@
+import "./map.scss"
+import React from 'react'
+import { useMemo } from "react";
+import { GoogleMap, Marker } from "@react-google-maps/api";
+
+
+
+
+const Map = () => {
+    const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+
+    return (
+      <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+        <Marker position={center} /> 
+      </GoogleMap>
+    ) 
+}
+
+export default Map
+
+
+
+
+
+
+
+
--- client/src/pages/promote/Promote.jsx
+++ client/src/pages/promote/Promote.jsx
@@ -3,7 +3,14 @@ import "./promote.scss";
 import Button from "../../components/ui/button/Button";
 import Input from "../../components/form elements/Input";
 
+import Map from "../../components/ui/map/Map";
+import { useLoadScript } from "@react-google-maps/api";
+
 const Promote = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBc4aQkvACIYYDgqY4Bt5DByygvOOyHQ7E",
+  });
+
   return (
     <div className="promote">
       <h1>Our Services / Offers</h1>
@@ -16,17 +23,19 @@ const Promote = () => {
           className="form-control"
         />
 
-        <Input
+        <div className="map-box">{!isLoaded ? <div>Loading...</div> : <Map />}</div>
+
+        <Input 
           element="input"
           type="number"
           placeholder="Radius from Location"
-          id="radiusfromlocation"
+          id="radiusfromlocation" 
           className="form-control"
         />
         <Input
           element="input"
           type="number"
-          onFocus={ (e) =>  e.target.type='date'}
+          onFocus={(e) => (e.target.type = "date")}
           placeholder="Promotion Period Date"
           id="promotionperioddate"
           className="form-control"
@@ -34,7 +43,7 @@ const Promote = () => {
         <Input
           element="input"
           type="text"
-          onFocus={ (e) =>  e.target.type='date'}
+          onFocus={(e) => (e.target.type = "date")}
           placeholder="Promotion Period Time"
           id="promotionperiodtime"
           className="form-control"
